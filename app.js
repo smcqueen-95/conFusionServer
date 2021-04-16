c5fa6040b0f898543f809bfe7f74c9b78e79ae41
Samira Mc Queen <samira.mcqueen11@gmail.com>
2021-04-16 14:05:23 -0400

    Express Session Part 2

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const mongoose = require("mongoose");
 const Dishes = require("./models/dishes");
 const Promotions = require("./models/promotions");
 const Leaders = require("./models/leaders");
+const Users = require("./models/user");
 
 const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
@@ -55,40 +56,23 @@ app.use(
   })
 );
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-      .toString()
-      .split(":");
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == "admin" && pass == "password") {
-      req.session.user = "admin";
-      next(); // authorized
-    } else {
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error("You are not authenticated!");
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.session.user === "admin") {
-      console.log("req.session: ", req.session);
+    if (req.session.user === "authenticated") {
       next();
     } else {
       var err = new Error("You are not authenticated!");
-      err.status = 401;
-      next(err);
+      err.status = 403;
+      return next(err);
     }
   }
 }
@@ -96,9 +80,6 @@ function auth(req, res, next) {
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
